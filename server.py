a8dab55163470b2188fb83bc32c8585796ef4a59
Iman Akbari Azirani <iakbaria@scspcxxx>
2019-04-11 01:49:52 -0400

    IDS events API

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python3
 
-from flask import Flask
+import json
+from snort import get_events
+from flask import Flask, request
+
 app = Flask("Hasley")
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/ids/events/")
+def events():
+    min_id = request.args.get('min_id', 0)
+    return json.dumps(list(get_events(min_id=min_id)))
+
 
 
