0abb5c781c91d3c7d4849a37708ec5f1dd1f3fd1
Iman Akbari <imakbari@gmail.com>
2019-05-02 21:06:22 -0400

    Update requirements

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev python3-dev
+sudo apt-get install -y build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev python3-dev inotify-tools python-virtualenv
 
 sudo apt-get install -y mysql-server libmysqlclient-dev
 
