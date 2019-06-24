b82fffe0169d57c9f2a2969a0795c3df9e1612eb
Iman Akbari <imakbari@gmail.com>
2019-06-24 11:15:28 -0400

    Complete run script

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-docker run -it --rm -v $(pwd)/src:/root/halsey iman/halsey:1
+DIR=$(dirname $0)
+
+portnum() {
+    cat src/halsey.yml | grep port | grep -oE '[[:digit:]]+'
+}
+
+port=$(portnum)
+daemon=""
+
+if [[ "$1" == "daemon" ]]
+then
+    daemon="-d"
+fi
+
+docker run $daemon -it --rm -p $port:$port -v $(pwd)/src:/root/halsey iman/halsey:1
